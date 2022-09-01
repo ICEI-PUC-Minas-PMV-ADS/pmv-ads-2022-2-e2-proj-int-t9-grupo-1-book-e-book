bde732802d5b1f553fd75724b3a767ebf91a7804
Jaynne <103083123+jnunesss@users.noreply.github.com>
2022-08-31 22:53:23 -0300

    chore: database initial structure

---
+++ src/init.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS "users" (
+"id"         SERIAL,
+"name"       VARCHAR(260)      NOT NULL,
+"email"      VARCHAR(260)      NOT NULL    UNIQUE,
+"password"   VARCHAR(260)      NOT NULL,
+PRIMARY KEY ("id")   
+);
+
+CREATE TABLE IF NOT EXISTS "books" (
+"id"          SERIAL,
+"name"        VARCHAR(260)      NOT NULL,
+"genre"       VARCHAR(260)      NOT NULL,
+PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "users_books" (
+"user_id"    INT      NOT NULL,
+"book_id"    INT      NOT NULL,
+FOREIGN KEY ("user_id")   REFERENCES   "users"(id)   ON DELETE CASCADE,
+FOREIGN KEY ("book_id")    REFERENCES  "books"(id)    ON DELETE CASCADE,
+PRIMARY KEY ("user_id", "book_id")
+);

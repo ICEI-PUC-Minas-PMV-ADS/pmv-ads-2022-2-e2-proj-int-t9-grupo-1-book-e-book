82a69d627d1f1359af4a4f2932a04aab2ad2dba8
Jaynne <103083123+jnunesss@users.noreply.github.com>
2022-09-01 00:05:14 -0300

    conexao base de dados

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import database from './config/database.js';
+import testConnection from './utils/check-db-connection.js';
+
+testConnection(database);
 
 const app = express();
 const port = process.env.APP_PORT;
+
 
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`)

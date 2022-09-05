c1fcf297a81c9775dff478c6d34f4f5f273a0470
Jaynne <103083123+jnunesss@users.noreply.github.com>
2022-09-05 20:39:25 -0300

    login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,31 +1,52 @@
-import database from '../config/database.js';
-import { NotFoundError } from '../utils/errors.js';
-import bcrypt from 'bcryptjs';
+import database from "../config/database.js";
+import { NotFoundError, BadRequestError } from "../utils/errors.js";
+import bcrypt from "bcryptjs";
 
 const getUsers = async (_, response, next) => {
-    try {
-        const { rowCount, rows } = await database().query('SELECT * FROM users');
-        if (rowCount === 0) throw new NotFoundError;
-        return response.status(200).json(rows);
-    } catch (error) {
-        next(error)
-    };
+  try {
+    const { rowCount, rows } = await database().query("SELECT * FROM users");
+    if (rowCount === 0) throw new NotFoundError();
+    return response.status(200).json(rows);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const registerUser = async (request, response, next) => {
-    try {
-        const { name, email, password } = request.body;
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-    
-        await database().query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3)', [name, email, hashedPassword]);
-        return response.sendStatus(201);
-    } catch (error) {
-        next(error)
-    };
-}
+  try {
+    const { name, email, password } = request.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-export {
-    getUsers,
-    registerUser
-}
+    await database().query(
+      "INSERT INTO users (name, email, password) VALUES ($1, $2, $3)",
+      [name, email, hashedPassword]
+    );
+    return response.sendStatus(201);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const loginUser = async (request, response, next) => {
+  try {
+    const { email, password } = request.body;
+    const { rows, rowCount } = await database().query(
+      "SELECT password FROM users WHERE email=$1",
+      [email]
+    );
+    if (rowCount === 0) throw new NotFoundError();
+
+    const userPassword = rows[0].password;
+    if (!(await bcrypt.compare(password, userPassword))) {
+        throw new BadRequestError('Senha incorreta')
+    }
+
+    //TODO: retornar jwt
+    return response.status(200).json(rows);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { getUsers, registerUser, loginUser };
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { getUsers, registerUser } from '../controllers/userController.js';
+import { getUsers, registerUser, loginUser } from '../controllers/userController.js';
 
 const getUserRouter = () => {
     const userRouter = express.Router();
     userRouter.get('/', getUsers);
     userRouter.post('/register', registerUser);
+    userRouter.post('/login', loginUser)
+
 
     return userRouter;
 }

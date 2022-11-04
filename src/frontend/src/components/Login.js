fc0e23b41912652327a0d1aedf32649d31177499
Jaynne <103083123+jnunesss@users.noreply.github.com>
2022-11-04 20:43:03 -0300

    correcao login

--- src/frontend/src/components/Login.js
+++ src/frontend/src/components/Login.js
@@ -9,10 +9,34 @@ export default function Login() {
 
   const navigate = useNavigate();
 
-  let handleSubmit = async (e) => {
+  const handleSubmitLogin = async (e) => {
     e.preventDefault();
     try {
-      console.log(name, email, password)
+      console.log(name, email, password);
+      let res = await fetch("http://localhost:4000/users/login", {
+        method: "POST",
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+      if (res.status === 201 || res.status === 201) {
+        setName("");
+        setEmail("");
+        setPassword("");
+        navigate("/books");
+      } else {
+        alert('Email ou senha incorretos')
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleSubmitRegister = async (e) => {
+    e.preventDefault();
+    try {
+      console.log(name, email, password);
       let res = await fetch("http://localhost:4000/users/register", {
         method: "POST",
         body: JSON.stringify({
@@ -25,9 +49,9 @@ export default function Login() {
         setName("");
         setEmail("");
         setPassword("");
-        navigate('/books')
+        navigate("/books");
       } else {
-        console.log(res)
+        alert('Ocorreu um erro no cadastro!')
       }
     } catch (err) {
       console.log(err);
@@ -36,33 +60,58 @@ export default function Login() {
 
   return (
     <div className="div-login">
-      <div >
+      <div>
         <h3>Faça seu cadastro</h3>
-        <form  className="div-login-inside"  onSubmit={handleSubmit}>
+        <form className="div-login-inside" onSubmit={handleSubmitRegister}>
           <label for="fname">Nome completo</label>
-          <input type="text" id="fname" name="name" onChange={(e) => setName(e.target.value)} />
+          <input
+            type="text"
+            id="fname"
+            name="name"
+            onChange={(e) => setName(e.target.value)}
+          />
           <br />
           <label for="fname">Email</label>
-          <input type="text" id="fname" name="email"  onChange={(e) => setEmail(e.target.value)} />
-          
+          <input
+            type="text"
+            id="fname"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
           <br />
           <label for="lname">Senha</label>
-          <input type="password" id="lname" name="password" onChange={(e) => setPassword(e.target.value)}/>
+          <input
+            type="password"
+            id="lname"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <br />
           <input type="submit" value="Submit" />
         </form>
       </div>
       <div>
-      <h3>Seja bem-vindo!</h3>
-      <form  className="div-login-inside" action="/">
-        <label for="fname">Email</label>
-        <input type="text" id="fname" name="email" />
-        <br />
-        <label for="lname">Senha</label>
-        <input type="text" id="lname" name="password" />
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
+        <h3>Seja bem-vindo!</h3>
+        <form className="div-login-inside" onSubmit={handleSubmitLogin}>
+          <label for="fname">Email</label>
+          <input
+            type="text"
+            id="fname"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <label for="lname">Senha</label>
+          <input
+            type="text"
+            id="lname"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     </div>
   );
